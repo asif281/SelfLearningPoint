d17569f2041834385703b3df50b5988e1574f824
asifs <asifs@LOCALMACHINE>
2022-01-29 08:30:29 +0500

    Add project files.

---
+++ DAL/Interfaces/ITeacher.cs
@@ -0,0 +1,13 @@
+﻿using DTOs;
+
+namespace DAL
+{
+    public interface ITeacher
+    {
+        List<TeacherDTO> displayTeacher();
+        void addTeacher(TeacherDTO teacherDTO);
+        void removeTeacher(int Id);
+        void updateTeacher(TeacherDTO teacherDTO);
+        TeacherDTO GetTeacherById(int Id);
+    }
+}
---
+++ DTOs/TeacherDTO.cs
@@ -0,0 +1,25 @@
+﻿using DomainModels.CustomValidation;
+using System.ComponentModel.DataAnnotations;
+
+namespace DTOs
+{
+    public class TeacherDTO
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        [SkillsValidate(Allowed = new string[] { "ASP.NET Core", "ASP.NET MVC", "ASP.NET Web Forms" }, ErrorMessage = "You skills are invalid")]
+        public string Skills { get; set; }
+
+        [Range(5, 50)]
+        public int TotalStudents { get; set; }
+
+        [Required]
+        public decimal Salary { get; set; }
+
+        public DateTime AddedOn { get; set; }
+    }
+}
