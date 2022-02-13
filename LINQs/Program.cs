c19d3790736c866b95e25500b575f7f1d15b2a4f
asif saeed <asifsaeed491@gmail.com>
2022-02-14 00:56:48 +0500

    1 Start the LINQ

--- LINQs/Program.cs
+++ LINQs/Program.cs
@@ -1,3 +1,36 @@
-// See https://aka.ms/new-console-template for more information
+    // Data source
+    using LINQs;
 
-Console.WriteLine("Hello, World!");
+    string[] names = { "Bill", "Steve", "James", "Mohan" };
+
+    // LINQ Query 
+    var myLinqQuery = from name in names
+                      where name.Contains("ja")
+                      select name;
+
+    // Query execution
+    foreach (var name in myLinqQuery)
+        Console.Write(name + " ");
+    // Let's Make an array of Student class type
+
+     Student[] studentArray =
+     { 
+          new Student() { Id = 1, age = 23, Name = "Asif"},
+          new Student() { Id = 2, age = 25, Name = "Aqib" },
+          new Student() { Id = 3, age = 27, Name = "Saqib" },
+          new Student() { Id = 4, age = 19, Name = "Asif"},
+          new Student() { Id = 5, age = 18, Name = "Aqib" },
+          new Student() { Id = 6, age = 14, Name = "Saqib" }
+     };
+
+    // Use LINQ to find teenager students
+    Student[] teenAgerStudents = studentArray.Where(s => s.age > 12 && s.age < 20).ToArray();
+        Console.Write(teenAgerStudents + " ");
+
+    // Use LINQ to find first student whose name is Bill 
+    Student bill = studentArray.Where(s => s.Name == "Bill").FirstOrDefault();
+        Console.Write(bill + " ");
+
+    // Use LINQ to find student whose StudentID is 5
+    Student student5 = studentArray.Where(s => s.Id == 5).FirstOrDefault();
+        Console.Write(student5 + " ");
---
+++ LINQs/Student.cs
@@ -0,0 +1,9 @@
+﻿namespace LINQs
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int age { get; set; }
+    }
+}
