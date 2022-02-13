88f2e98f83c03388b7bfcbb11aa8816ac2b267e4
asif saeed <asifsaeed491@gmail.com>
2022-02-13 23:18:06 +0500

    Refine the Model Code

--- DomainModels/CustomValidation/SkillsValidate.cs
+++ DomainModels/CustomValidation/SkillsValidate.cs
@@ -8,12 +8,14 @@ namespace DomainModels.CustomValidation
 {
     public class SkillsValidate : Attribute, IModelValidator
     {
-        public string[] Allowed { get; set; }
-        public string ErrorMessage { get; set; }
+        public string[]? Allowed { get; set; }
+        public string? ErrorMessage { get; set; }
         public IEnumerable<ModelValidationResult> Validate(ModelValidationContext context)
         {
 
+#pragma warning disable CS8604 // Possible null reference argument.
             if (Allowed.Contains(context.Model as string))
+#pragma warning restore CS8604 // Possible null reference argument.
                 return Enumerable.Empty<ModelValidationResult>();
             else
                 return new List<ModelValidationResult> {
--- DomainModels/Teacher.cs
+++ DomainModels/Teacher.cs
@@ -8,11 +8,11 @@ namespace DomainModels
         public int Id { get; set; }
 
         [Required]
-        public string Name { get; set; }
+        public string? Name { get; set; }
 
         [Required]
         [SkillsValidate(Allowed = new string[] { "ASP.NET Core", "ASP.NET MVC", "ASP.NET Web Forms" }, ErrorMessage = "You skills are invalid")]
-        public string Skills { get; set; }
+        public string? Skills { get; set; }
 
         [Range(5, 50)]
         public int TotalStudents { get; set; }
